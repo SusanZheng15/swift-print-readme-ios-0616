761d4e18cb7a8e692ec57db586cc353b18607e2d
SusanZheng15 <zheng.susan12@gmail.com>
2016-07-05 12:37:19 -0400

    Done

--- Print.playground/Contents.swift
+++ Print.playground/Contents.swift
@@ -10,10 +10,10 @@ print("I'm printing a string in Swift!")
 
 print("Hi Mom! My favorite word is serendipity, my favorite color is green, and my favorite musician is Billy Joel.")
 
-let color = "blue"
-print("The sky is \(color).")
+let color = "blue" //constant variable, cannot be changed
+print("The sky is \(color).") //prints the sky is blue
 
 print("Hi Mom! My favorite word is \(favoriteWord), my favorite color is \(favoriteColor), and my favorite musician is \(favoriteMusician).")
 
 var skySentence = "The sky is \(color)."
-print(skySentence)
+print(skySentence) //prints "the sky is blue"
